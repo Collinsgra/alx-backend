7980b030839b3d703dd1c230c0523b161d65f4db
Collinsgra <kipkemoicollins941@gmaill.com>
2024-07-18 10:28:06 -0700

    basuic cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,18 +1,24 @@
-#!/usr/bin/python3
-
-""" Basic """
-
+#!/usr/bin/env python3
+""" BaseCaching
+"""
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """Basic """
+    """Create a class BasicCache
+    """
+    def __init__(self):
+        """ inititialize function"""
+        super().__init__()
 
     def put(self, key, item):
-        """Puts in cache"""
+        """add items to dictionary"""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Gets from cache"""
-        return self.cache_data.get(key, None)
+        """ get items from dictionary"""
+        if key in self.cache_data.keys():
+            return self.cache_data[key]
+        else:
+            return None
